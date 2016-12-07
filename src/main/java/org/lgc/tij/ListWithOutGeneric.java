55aadd1aca1cfad0df997448772411b8650cbcc6
lgc13136@126.com <lgc13136@126.com>
2016-12-07 22:02:18 +0800

    修改作者

--- src/main/java/org/lgc/tij/ListWithOutGeneric.java
+++ src/main/java/org/lgc/tij/ListWithOutGeneric.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
  * @Package: org.lgc.tij
  * @Description: TODO (这里用一句话描述这个类的作用)
  *
- * @Author: laigc
+ * @Author: lgc
  * @Date: 2016年12月7日 上午8:13:43
  *
  *        Copyright @ 2016 Corpration Name
