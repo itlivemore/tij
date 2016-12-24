4f24f868dd7f202446d5369829a2ab3f60e8dc67
lgc13136 <lgc13136@126.com>
2016-12-24 16:19:18 +0800

    自定义异常

---
+++ src/main/java/org/lgc/tij/exceptions/FullConstructors.java
@@ -0,0 +1,39 @@
+package org.lgc.tij.exceptions;
+
+/**
+ * Created by laigc on 2016/12/24.
+ */
+class MyException extends Exception {
+    public MyException() {
+
+    }
+    public MyException(String msg) {
+        super(msg);
+    }
+}
+
+public class FullConstructors {
+    public static void f() throws MyException{
+        System.out.println("Throwing MyException from f()");
+        throw new MyException();
+    }
+
+    public static void g() throws MyException{
+        System.out.println("Throwing MyException from g()");
+        throw new MyException("Originated in g()");
+    }
+
+    public static void main(String[] args) {
+        try {
+            f();
+        } catch (MyException e) {
+            // 输出到System.out，从控制台结果来看，这里是默认的颜色，e.printStackTrace是红色
+            e.printStackTrace(System.out);
+        }
+        try {
+            g();
+        } catch (MyException e) {
+            e.printStackTrace(); // 输出到标准错误流
+        }
+    }
+}
