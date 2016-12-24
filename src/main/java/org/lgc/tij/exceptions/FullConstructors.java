707a8100c146e426ed77da38917ed729cc5abbd7
lgc13136 <lgc13136@126.com>
2016-12-24 16:19:32 +0800

    自定义异常

--- src/main/java/org/lgc/tij/exceptions/FullConstructors.java
+++ src/main/java/org/lgc/tij/exceptions/FullConstructors.java
@@ -7,18 +7,19 @@ class MyException extends Exception {
     public MyException() {
 
     }
+
     public MyException(String msg) {
         super(msg);
     }
 }
 
 public class FullConstructors {
-    public static void f() throws MyException{
+    public static void f() throws MyException {
         System.out.println("Throwing MyException from f()");
         throw new MyException();
     }
 
-    public static void g() throws MyException{
+    public static void g() throws MyException {
         System.out.println("Throwing MyException from g()");
         throw new MyException("Originated in g()");
     }
