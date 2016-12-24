fb54709249a8f7ff76680ae0ee3f120bbbdd95a9
lgc13136 <lgc13136@126.com>
2016-12-24 16:54:41 +0800

    添加注释

--- src/main/java/org/lgc/tij/exceptions/LoggingExceptions2.java
+++ src/main/java/org/lgc/tij/exceptions/LoggingExceptions2.java
@@ -4,13 +4,13 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.logging.Logger;
 
-/**
+/**记录其他人的异常信息
  * Created by laigc on 2016/12/24.
  */
 public class LoggingExceptions2 {
     private static Logger logger = Logger.getLogger(LoggingExceptions2.class.getName());
 
-    static void logException(Exception e) {
+    static void logException(Exception e) { // 记录异常信息
         StringWriter trace = new StringWriter();
         e.printStackTrace(new PrintWriter(trace));
         logger.severe(trace.toString());
