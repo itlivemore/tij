ee2829ea1a5b87400dacf7aad7ab33be2544ca77
lgc13136 <lgc13136@126.com>
2016-12-24 16:51:15 +0800

    记录异常日志

---
+++ src/main/java/org/lgc/tij/exceptions/LoggingExceptions2.java
@@ -0,0 +1,27 @@
+package org.lgc.tij.exceptions;
+
+import net.mindview.util.CountingGenerator;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.Logger;
+
+/**
+ * Created by laigc on 2016/12/24.
+ */
+public class LoggingExceptions2 {
+    private static Logger logger = Logger.getLogger(LoggingExceptions2.class.getName());
+    static void logException(Exception e) {
+        StringWriter trace = new StringWriter();
+        e.printStackTrace(new PrintWriter(trace));
+        logger.severe(trace.toString());
+    }
+
+    public static void main(String[] args) {
+        try {
+            throw new NullPointerException();
+        } catch (NullPointerException e) {
+            logException(e);
+        }
+    }
+}
