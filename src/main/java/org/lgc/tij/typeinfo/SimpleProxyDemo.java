f381fca935f76ba0a82d66db7d35153b0382f2ae
lgc13136 <lgc13136@126.com>
2017-01-01 19:39:42 +0800

    简单代理

---
+++ src/main/java/org/lgc/tij/typeinfo/SimpleProxyDemo.java
@@ -0,0 +1,56 @@
+package org.lgc.tij.typeinfo;
+
+/**
+ * 代理
+ * Created by laigc on 2017/1/1.
+ */
+interface Interface {
+    void doSomething();
+
+    void somethingElse(String arg);
+}
+
+class RealObject implements Interface {
+
+    @Override
+    public void doSomething() {
+        System.out.println("doSomething");
+    }
+
+    @Override
+    public void somethingElse(String arg) {
+        System.out.println("somethingElse " + arg);
+    }
+}
+
+class SimpleProxy implements Interface {
+    private Interface proxied;
+
+    public SimpleProxy(Interface proxied) {
+        this.proxied = proxied;
+    }
+
+    @Override
+    public void doSomething() {
+        System.out.println("SimpleProxy doSomething");
+        proxied.doSomething();
+    }
+
+    @Override
+    public void somethingElse(String arg) {
+        System.out.println("SimpleProxy somethingElse " + arg);
+        proxied.somethingElse(arg);
+    }
+}
+
+public class SimpleProxyDemo {
+    public static void consumer(Interface iface) {
+        iface.doSomething();
+        iface.somethingElse("hello");
+    }
+
+    public static void main(String[] args) {
+        consumer(new RealObject());
+        consumer(new SimpleProxy(new RealObject()));
+    }
+}
