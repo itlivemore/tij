41b6e383c3eeeb3109ace87e3bed91ad5841432c
lgc13136 <lgc13136@126.com>
2017-01-01 11:29:16 +0800

    list添加newInstance生成的对象

---
+++ src/main/java/org/lgc/tij/typeinfo/FilledList.java
@@ -0,0 +1,45 @@
+package org.lgc.tij.typeinfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 生成一个List，通过Class的newInstance()方法生成对象加入到List中
+ * Created by laigc on 2017/1/1.
+ */
+class CountedInteger {
+    private static long counter;
+    private final long id = counter++;
+
+    @Override
+    public String toString() {
+        return Long.toString(id);
+    }
+}
+
+public class FilledList<T> {
+    private Class<T> type;
+
+    public FilledList(Class<T> type) {
+        this.type = type;
+    }
+
+    public List<T> create(int nElements) {
+        List<T> result = new ArrayList<T>();
+        try {
+            for (int i = 0; i < nElements; i++) {
+                result.add(type.newInstance());
+            }
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        FilledList<CountedInteger> fl = new FilledList<>(CountedInteger.class);
+        System.out.println(fl.create(15));
+    }
+}
