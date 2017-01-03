be05d69a9d7a232ba6251c2e149ceb411bbbd691
lgc13136@126.com <lgc13136@126.com>
2017-01-03 22:07:13 +0800

    提交Watercolor

--- src/main/java/org/lgc/tij/generics/watercolors/WaterColorSets.java
+++ src/main/java/org/lgc/tij/generics/watercolors/WaterColorSets.java
@@ -11,12 +11,12 @@ import java.util.Set;
  */
 public class WaterColorSets {
     public static void main(String[] args) {
-        Set<Watercolor> set1 = EnumSet.range(Watercolor.BRILLIANT_RED, Watercolor.VIRIDIAN_HUE);
-        Set<Watercolor> set2 = EnumSet.range(Watercolor.CERULEAN_BLUE_HUE, Watercolor.BURNT_UMBER);
+        Set<Watercolors> set1 = EnumSet.range(Watercolors.BRILLIANT_RED, Watercolors.VIRIDIAN_HUE);
+        Set<Watercolors> set2 = EnumSet.range(Watercolors.CERULEAN_BLUE_HUE, Watercolors.BURNT_UMBER);
         System.out.println("set1: " + set1);
         System.out.println("set2: " + set2);
         System.out.println(Sets.union(set1, set2));// 并集
-        Set<Watercolor> subSet = Sets.intersection(set1, set2);// 交集
+        Set<Watercolors> subSet = Sets.intersection(set1, set2);// 交集
         System.out.println(subSet);
         System.out.println(Sets.difference(set2, subSet));// 从set2中移除subSet中包含的元素
         System.out.println(Sets.complement(set1, set2)); // 返回set1中包含除了交集之外的所有元素
